491b8891833609ec342a0a2e506a2457507977d2
Spencer Boucher <github@spencerboucher.com>
2018-07-19 00:56:25 -0400

    Remove annoying things from modeline

--- local/.emacs.d/init.el
+++ local/.emacs.d/init.el
@@ -293,7 +293,50 @@
   :ensure spaceline
   :config
   (spaceline-helm-mode)
-  (spaceline-spacemacs-theme))
+  (spaceline-compile
+    `(((persp-name
+        workspace-number
+        window-number)
+       :fallback evil-state
+       :face highlight-face
+       :priority 100)
+      (anzu :priority 95)
+      auto-compile
+      ((buffer-modified
+        buffer-size
+        buffer-id
+        remote-host)
+       :priority 98)
+      (major-mode :priority 79)
+      (process :when active)
+      ((flycheck-error
+        flycheck-warning
+        flycheck-info)
+       :when active
+       :priority 89)
+      (minor-modes
+       :when active
+       :priority 9)
+      (mu4e-alert-segment :when active)
+      (erc-track :when active)
+      (version-control
+       :when active
+       :priority 78)
+      (org-pomodoro :when active)
+      (org-clock :when active))
+    `(which-function
+      (python-pyvenv :fallback python-pyenv)
+      (purpose :priority 94)
+      (battery :when active)
+      (selection-info :priority 95)
+      ((point-position
+        line-column)
+       :separator " | "
+       :priority 96)
+      (global :when active)
+      (buffer-position :priority 99)
+      (hud :priority 99)))
+  (setq-default mode-line-format '("%e" (:eval (spaceline-ml-main)))))
 
 ;; other packages
 
